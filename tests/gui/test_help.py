446215d1412ee579fad8ebc005b82ce5dcbe8d6d
schragelab <schragelab@education.wisc.edu>
2021-11-03 07:16:42 -0700

    removed unnecessary files for CLI/module use

--- tests/gui/test_help.py
+++
@@ -1,9 +0,0 @@
-from dicomsort.gui.dialogs import HelpDlg
-from dicomsort.gui.overrides import HtmlWindow
-from tests.shared import WxTestCase
-
-
-class TestHelpDialog(WxTestCase):
-    def test_constructor(self):
-        self.dlg = HelpDlg(self.frame)
-        assert isinstance(self.dlg.hwin, HtmlWindow)
